d0c4d2fc8b757844277450b137ab9c48db2dfec9
kauazica <kauajamariqueli150508@gmail.com>
2025-10-29 22:55:02 -0300

    fim part1

---
+++ components/ui/spinner.tsx
@@ -0,0 +1,6 @@
+// components/ui/spinner.tsx
+export function Spinner() {
+  return (
+    <div className="w-5 h-5 border-2 border-t-transparent border-gray-500 rounded-full animate-spin"></div>
+  );
+}
